f6a27f9126658f3dc065ac23f531c1aad7d1cab2
matin0x <raymart.vills@gmail.com>
2022-11-21 03:56:15 +0800

    Setup listing API, db_config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+// const db = require('./database')
 //app.use(express.static("public"))
 
 app.get("/", (req,res)=>{ // home page
@@ -11,35 +11,16 @@ app.get("/results", (req,res)=>{ // results page
     res.sendFile('public/results.html',{root: __dirname})
 })
 
+const mysql = require('mysql2');
+const config = require('./db_config');
+const db = mysql.createConnection(config.db);
+
 app.get("/api/listings", (req,res)=>{ // test api
-    if (req.query.key==='sunrise') //get query keyword
-        res.json([{name: "sunrise dormitories", address: "street", area: 20, pricing: 5000},{name: "sunrise 2 dormitories", address: "street", area: 20, pricing: 5000}])
-    res.end()
+  let sql = `SELECT * FROM properties`;
+  db.query(sql, function(err, data, fields) {
+    if (err) throw err;
+    res.json({data})
+  })
 })
-
-// Database Connection
-
-const mysql = require('mysql2')
-const connection = mysql.createConnection({
-  // Hardcoded Values
-  host: 'winhost',                  //replace with SQL server host
-  user: 'wsl_root',                 //replace with SQL server username
-  password: 'password',             
-  database: 'alternative_housing',
-  port: 3306
-})
-
-connection.connect()
-
-// Sample Query
-connection.query(
-    'SELECT * FROM `properties` ',
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-    //   console.log(fields); // fields contains extra meta data about results, if available
-    }
-  );
-
-connection.end()
 
 app.listen(3000)
