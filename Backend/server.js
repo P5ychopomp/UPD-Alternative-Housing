d017e0ab8c87d3856a7fb16a5baac0c7573a4c5d
adzmco23 <112993167+adzmco23@users.noreply.github.com>
2023-03-16 21:51:23 +0800

    refactored sql statement construction

--- Backend/server.js
+++ Backend/server.js
@@ -26,20 +26,151 @@ app.get("/api/listings", queryResults, (req,res)=>{
 })
 
 // property details request given property_id
-app.get("/api/listings/:id", queryProperty, (req,res)=>{
+app.get("/api/listings/:id", queryResults, (req,res)=>{
     db.query(req.sql, req.queryFilters, function(err, data, fields) {
         if (err) throw err;
         res.json({data})
     })
 })
 
-// prevents SQL injection using special characters
-function noSpecialCharacters(data){
-    if(c = data.match(/[`'/\*%;\+\|\<\>=!\.\-]/)){ // special characters to avoid
-        console.log(`Illegal character found: ${c} in ${data}`)
-        return 0;
+class queryField{
+    constructor(filter, value){
+        this.filter=filter;
+        this.value=value;
     }
-    return 1;
+
+    getValue(){
+        return this.value;
+    }
+
+    getFormatted(){
+        return this.value;
+    }
+
+    getFilter(){
+        return this.filter;
+    }
+}
+
+class keyword extends queryField{
+    constructor(value){
+        super(" AND (property_name LIKE ? OR street_address LIKE ? OR brgy LIKE ? OR city_municip LIKE ?)", value);
+    }
+    getFormatted(){
+        return ["%"+this.value+"%","%"+this.value+"%","%"+this.value+"%","%"+this.value+"%"];
+    }
+}
+
+class ratemin extends queryField{
+    constructor(value){
+        super(" AND rate >= ?", value);
+    }
+}
+
+class ratemax extends queryField{
+    constructor(value){
+        super(" AND rate <= ?", value);
+    }
+}
+
+class lotmin extends queryField{
+    constructor(value){
+        super(" AND lot_area >= ?", value);
+    }
+}
+
+class lotmax extends queryField{
+    constructor(value){
+        super(" AND lot_area <= ?", value);
+    }
+}
+
+class furnished extends queryField{
+    constructor(value){
+        super(" AND furnishing = ?", value);
+    }
+}
+class curfew extends queryField{
+    constructor(value){
+        super(" AND curfew = ?", value);
+    }
+}
+class type extends queryField{
+    constructor(value){
+        super(" AND lot_type = ?", value);
+    }
+}
+class occupancy extends queryField{
+    constructor(value){
+        super(value==1 ? " AND occupancy > 1" : " AND occupancy <= 1", value);
+    }
+}
+class stay extends queryField{
+    constructor(value){
+        super(" AND min_month_stay >= ? AND min_month_stay <= ?", value);
+    }
+}
+class amenities extends queryField{
+    constructor(value){
+        super("", value);
+    }
+}
+class propertyID extends queryField{
+    constructor(value){
+        super(" AND property_id = ?", value);
+    }
+}
+
+class sqlQuery{
+    sql="SELECT * FROM properties WHERE 1";
+    queryFilters=[];
+    limit = " LIMIT 20";
+
+    constructor(fields){
+        this.req = fields;
+        this.q = new keyword(fields.q);
+        this.ratemin = new ratemin(fields.ratemin);
+        this.ratemax=new ratemax(fields.ratemax); 
+        this.lotmin=new lotmin(fields.lotmin);
+        this.lotmax=new lotmax(fields.lotmax);
+        this.furnished=new furnished(["None","Semi","Full"][fields.furnished%3]); 
+        this.curfew=new curfew(fields.curfew);
+        this.type=new type(["Condominium", "Dormitory", "Apartment", "Boarding House"][fields.type%4]);
+        this.occupancy=new occupancy([]);
+        this.stay=new stay([[0,7,13][fields.stay], [6,12,24][fields.stay]]);
+        this.a=new amenities(fields.a);
+
+        this.id=new propertyID(fields.id);
+    }
+
+    noSpecialCharacters(data){
+        let c='';
+        if(c = data.match(/[`'/\*%;\+\|\<\>=!\.\-]/)){ // special characters to avoid
+            console.log(`Illegal character found: ${c} in ${data}`)
+            return 0;
+        }
+        return 1;
+    }
+
+    build(){
+        for (let key of Object.keys(this.req)){    // append each filter parameter in request to sql statement
+            // guard clause against special characters
+            if (!this.noSpecialCharacters(String(this[key].getValue()))){
+                return 400;
+            }
+            this.queryFilters=this.queryFilters.concat(this[key].getFormatted()); 
+            this.sql+=this[key].getFilter();
+        }
+        this.sql+=this.limit; // add limit to db query result
+    }
+
+    getSQL(){
+        return this.sql;
+    }
+
+    getFilter(){
+        return this.queryFilters;
+    }
 }
 
 //const filters = new Set(["q","ratemin","ratemax","lotmin","lotmax","furnished","curfew","type","occupancy","stay","a"]);
@@ -47,89 +178,12 @@ function noSpecialCharacters(data){
 // set up query for list of properties given set of filters
 // input validation: allowlisting, placeholders, illegal characters
 function queryResults(req, res, next){
-    req.sql = 'SELECT * FROM properties WHERE 1' // initial sql statement
-    req.queryFilters=[]
-
-    // loop through query array to build sql statement? is this safe?
-    for (let key of Object.keys(req.query)){    // for each filter parameter in request
-
-        // guard clause against special characters
-        if (!noSpecialCharacters(req.query[key])){
-            return res.status(400).json({ err: "Bad Request"});
-        }
-
-        // for each valid filter, add search key and value to sql statement
-        switch(key) {
-            case "q":
-                req.sql += " AND (property_name LIKE ? OR street_address LIKE ? OR brgy LIKE ? OR city_municip LIKE ?)"
-                req.query.q="%" + req.query.q + "%"
-                req.queryFilters.push(req.query[key])
-                req.queryFilters.push(req.query[key])
-                req.queryFilters.push(req.query[key])
-                req.queryFilters.push(req.query[key])
-                break;
-            case "ratemin":       
-                req.sql += " AND rate >= ?"   // append filter to WHERE clause
-                req.queryFilters.push(req.query[key])
-                break;          
-            case "lotmin":
-                req.sql += " AND lot_area >= ?"   // append filter to WHERE clause
-                req.queryFilters.push(req.query[key])
-                break;
-            case "ratemax":
-                req.sql += " AND rate <= ?"
-                req.queryFilters.push(req.query[key])
-                break;
-            case "lotmax":
-                req.sql += " AND lot_area <= ?"
-                req.queryFilters.push(req.query[key])
-                break;
-            case "furnished":
-                req.sql += " AND furnishing = ?"
-                req.queryFilters.push(["None","Semi","Full"][(req.query[key])%3])
-                break;
-            case "curfew":
-                req.sql += " AND curfew = ?"
-                req.queryFilters.push(req.query[key])
-                break;
-            case "type":
-                req.sql += " AND lot_type = ?"
-                req.queryFilters.push(["Condominium", "Dormitory", "Apartment", "Boarding House"][(req.query[key])%4])
-                break;
-            case "occupancy":
-                req.sql += req.query[key]==1 ? " AND occupancy > 1" : " AND occupancy <= 1"
-                break;
-            case "stay":
-                req.sql += " AND min_month_stay >= ? AND min_month_stay <= ?"
-                req.queryFilters.push([0,7,13][req.query[key]])
-                req.queryFilters.push([6,12,24][req.query[key]])
-                break;
-            case "inclusions":
-                // set operations?
-                break;
-            default: // key not in allowed filters
-                console.log(`Illegal key: ${key}`)
-                return res.status(400).json({ err: "Bad Request"});
-        }
-    }
-
-    req.sql += " LIMIT 20"
-
-    // log sql query statement
-    console.log('request validated.')
-    console.log(req.sql)      
-    console.log(req.queryFilters)
-    next();
-}
-
-// set up query for specific property given property_id
-// input validation: placeholders, illegal characters
-function queryProperty(req, res, next){ 
-    req.sql = 'SELECT * FROM properties WHERE property_id = ?' // initial sql statement
-    if (!noSpecialCharacters(req.params.id)){
+    let sql = new sqlQuery({...req.query, ...req.params}); // include req.params
+    if (sql.build()==400){
         return res.status(400).json({ err: "Bad Request"});
     }
-    req.queryFilters=[req.params.id]
+    req.sql=sql.getSQL();
+    req.queryFilters=sql.getFilter();
 
     // log sql query statement
     console.log('request validated.')
