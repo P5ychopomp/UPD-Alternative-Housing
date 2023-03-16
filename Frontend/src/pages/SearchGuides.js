ed5ea611cf1f6e4d18dcc1a8cfd53cd0ba591648
P5ychopomp <35238879+P5ychopomp@users.noreply.github.com>
2023-03-16 20:38:20 +0800

    Fixed illegal char in search (#6)
    * Fixed illegal char in search
    
    * Fixed navbar resizing

--- Frontend/src/pages/SearchGuides.js
+++ Frontend/src/pages/SearchGuides.js
@@ -1,13 +1,14 @@
-import { Box, Container } from '@chakra-ui/react'
-import React from 'react'
+import { Box, ChakraProvider, Container } from '@chakra-ui/react'
 
 const SearchGuides = () => {
   return (
-    <Container mt='60' alignItems='center' centerContent>
-      <Box mb='50'>
+    <ChakraProvider>
+      <Container mt='20' alignItems='center' centerContent>
+      <Box mb='50' fontSize='28' fontWeight='bold' alignItems='center' >
         <h1>Search Guides</h1>
       </Box>
     </Container>
+    </ChakraProvider>
   )
 }
 
