c1bb2a47f632dc5464f01681f4c7691e6f7c60bf
P5ychopomp <dnvr.bugaoisan@gmail.com>
2023-04-18 04:01:14 +0800

    fix: auth

--- Backend/server.js
+++ Backend/server.js
@@ -9,7 +9,7 @@ var ensureLogIn = require('connect-ensure-login').ensureLoggedIn;
 var ensureLoggedIn = ensureLogIn();
 
 var cors = require('cors');
-app.use(cors({credentials: true, origin: ,"https://upd-alternative-housing.vercel.app/", optionsSuccessStatus: 200}));
+app.use(cors({credentials: true, origin: "https://upd-alternative-housing.vercel.app/", optionsSuccessStatus: 200}));
 app.use(express.urlencoded({ extended: false }));
 
 // Session Storage
