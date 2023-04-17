e932eb22c0f93e97b2e8ab2c8f20f11b552c1714
P5ychopomp <dnvr.bugaoisan@gmail.com>
2023-04-17 10:46:09 +0800

    Fix: cors

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -12,10 +12,6 @@ app.use(cors({credentials: true, origin: 'http://localhost:3000', optionsSuccess
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     next();
   });
 
