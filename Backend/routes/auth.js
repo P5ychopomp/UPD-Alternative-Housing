3ccc1c84caa20a7be03eab203b971ee8f078bcd4
P5ychopomp <dnvr.bugaoisan@gmail.com>
2023-04-18 04:20:56 +0800

    fix: auth

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -7,63 +7,77 @@ var LocalStrategy = require("passport-local");
 const bcrypt = require("bcrypt");
 var pool = require("../db_config").pool;
 
-var cors = require('cors');
-app.use(cors({credentials: true, origin: "https://upd-alternative-housing.vercel.app", optionsSuccessStatus: 200}));
+var cors = require("cors");
+app.use(
+  cors({
+    credentials: true,
+    origin: ["https://upd-alternative-housing.vercel.app", "http://localhost:3000"],
+    optionsSuccessStatus: 200,
+  })
+);
 
 function initializePassport(passport) {
-    console.log("Initialized");
+  console.log("Initialized");
 
-    // Use passport-local strategy (cookies and session)
-    const authenticateUser = (email, password, done) => {
-        console.log(email, password);
-        pool.query(`SELECT * FROM accounts WHERE email = ?`, [email],
-          (err, results) => {
-            if (err) { throw err; }
-            console.log(results);
-    
-            if (results.length > 0) {
-              const user = results[0];
-              bcrypt.compare(password, user.password, (err, isMatch) => {
-                if (err) { console.log(err); }
-                if (isMatch) { 
-                  return done(null, user);
-                } else {
-                  //password is incorrect
-                  return done(null, false, { message: "Password is incorrect" });
-                }
-              });
-            } else {
-              // No user
-              return done(null, false, { message: "No user with that email address"});
+  // Use passport-local strategy (cookies and session)
+  const authenticateUser = (email, password, done) => {
+    console.log(email, password);
+    pool.query(
+      `SELECT * FROM accounts WHERE email = ?`,
+      [email],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        console.log(results);
+
+        if (results.length > 0) {
+          const user = results[0];
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) {
+              console.log(err);
             }
-          }
-        );
-      };
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              //password is incorrect
+              return done(null, false, { message: "Password is incorrect" });
+            }
+          });
+        } else {
+          // No user
+          return done(null, false, {
+            message: "No user with that email address",
+          });
+        }
+      }
+    );
+  };
 
-    passport.use(
-        new LocalStrategy(
-          { usernameField: "email", passwordField: "password" },
-          authenticateUser
-        )
-      );
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "email", passwordField: "password" },
+      authenticateUser
+    )
+  );
 
-    /* Configure session management.
-    *
-    * When a login session is established, information about the user will be
-    * stored in the session supplied by the `serializeUser` function, 
-    * which is yielding the user ID and username.
-    *
-    * As the user interacts with the app, subsequent requests will be authenticated
-    * by verifying the session.  The same user information that was serialized at
-    * session establishment will be restored when the session is authenticated by
-    * the `deserializeUser` function.
-    *
-    * Since every request to the app needs the user ID and username, in order to
-    * fetch records and show the user element, that information is stored in the session.
-    */
-    passport.serializeUser((user, done) => done(null, user.id));
+  /* Configure session management.
+   *
+   * When a login session is established, information about the user will be
+   * stored in the session supplied by the `serializeUser` function,
+   * which is yielding the user ID and username.
+   *
+   * As the user interacts with the app, subsequent requests will be authenticated
+   * by verifying the session.  The same user information that was serialized at
+   * session establishment will be restored when the session is authenticated by
+   * the `deserializeUser` function.
+   *
+   * Since every request to the app needs the user ID and username, in order to
+   * fetch records and show the user element, that information is stored in the session.
+   */
+  passport.serializeUser((user, done) => done(null, user.id));
 
-    passport.deserializeUser((id, done) => {
+  passport.deserializeUser((id, done) => {
     pool.query(`SELECT * FROM accounts WHERE id = ?`, [id], (err, results) => {
       if (err) {
         return done(err);
@@ -84,95 +98,101 @@ router.use(passport.session());
 /**** USER REGISTRATION ****/
 router.get("/registration", (req, res) => {
   //! Unsure how to handle with React
-	res.sendFile("temp_registration.html", { root: __dirname });
+  res.sendFile("temp_registration.html", { root: __dirname });
 });
 
 router.post("/register", async (req, res) => {
   let { first_name, last_name, email, password, passwordconfirm } = req.body;
 
-	let errors = [];
-  
-	console.log({
-    first_name, 
+  let errors = [];
+
+  console.log({
+    first_name,
     last_name,
-		email,
-		password,
-		passwordconfirm,
-	});
+    email,
+    password,
+    passwordconfirm,
+  });
 
   // Second layer of form validation; may be ommited
   // Redirect the POST request to registration page when form is invalid
   // TODO: Implement better handling of invalid form
-	if (!first_name || !last_name || !email || !password || !passwordconfirm) {
+  if (!first_name || !last_name || !email || !password || !passwordconfirm) {
     errors.push({ message: "Please enter all fields" });
     res.redirect("/registration");
-	}
-  
-	if (password !== passwordconfirm) {
-    errors.push({ message: "Passwords do not match" });
-    res.redirect("/registration");
-	}
-  
-  // Password must be atleast 6 characters with one digit and special character
-  let strongPassword = new RegExp('(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
-  
-  if (!strongPassword.test(password)) {
-    // Error 201: Weak Password
-    res.send({message: 201});
   }
 
-	if (errors.length > 0) {
-		console.log(errors);
-	} else {
-		hashedPassword = await bcrypt.hash(password, 10);
-		console.log(hashedPassword);
-		// Validation passed
-		pool.query(
-			`SELECT * FROM accounts WHERE email = ?`, [email],
-			(err, results) => {
-				if (err) {
-					console.log(err);
-				}
-				console.log(results);
+  if (password !== passwordconfirm) {
+    errors.push({ message: "Passwords do not match" });
+    res.redirect("/registration");
+  }
 
-				if (results.length > 0) {
+  // Password must be atleast 6 characters with one digit and special character
+  let strongPassword = new RegExp(
+    "(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})"
+  );
+
+  if (!strongPassword.test(password)) {
+    // Error 201: Weak Password
+    res.send({ message: 201 });
+  }
+
+  if (errors.length > 0) {
+    console.log(errors);
+  } else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // Validation passed
+    pool.query(
+      `SELECT * FROM accounts WHERE email = ?`,
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(results);
+
+        if (results.length > 0) {
           // Error 202 : Account already used
-					res.send({message: 202});
-				} else {
-					console.log("Inserting to database...");
-					console.log({ first_name, last_name, email, hashedPassword });
-					pool.query(
-						`INSERT INTO accounts (first_name, last_name, email, password, created) VALUES (?,?,?,?, NOW())`,
-						// RETURNING id, password`,      <---Postgresql Keyword
-						[first_name, last_name, email, hashedPassword],
-						(err, results) => {
-							if (err) {
-								throw err;
-							}
-							console.log("You are now registered. Please log in");
+          res.send({ message: 202 });
+        } else {
+          console.log("Inserting to database...");
+          console.log({ first_name, last_name, email, hashedPassword });
+          pool.query(
+            `INSERT INTO accounts (first_name, last_name, email, password, created) VALUES (?,?,?,?, NOW())`,
+            // RETURNING id, password`,      <---Postgresql Keyword
+            [first_name, last_name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log("You are now registered. Please log in");
               // Success 200: Email Registered
-              res.send({message: 200});
-						}
-					);
-				}
-			}
-		);
-	}
+              res.send({ message: 200 });
+            }
+          );
+        }
+      }
+    );
+  }
 });
 
 /**** USER LOGIN ****/
-router.get("/login", checkAuthenticated, (req, res) => { // results page
+router.get("/login", checkAuthenticated, (req, res) => {
+  // results page
   if (req.user) {
     res.send({ loggedIn: true, user: req.session.user });
   } else {
-    console.log("GET /login")
+    console.log("GET /login");
     res.send({ loggedIn: false });
   }
-})
+});
 
 // Login page for backend testing
-router.get("/login_page", checkAuthenticated, (req,res)=>{ // results page
-  res.sendFile('temp_login.html',{root: __dirname})})
+router.get("/login_page", checkAuthenticated, (req, res) => {
+  // results page
+  res.sendFile("temp_login.html", { root: __dirname });
+});
 
 /* router.post("/login", function(req, res, next) {
     passport.authenticate('local', function(err, user, info) {
@@ -186,38 +206,39 @@ router.get("/login_page", checkAuthenticated, (req,res)=>{ // results page
       res.send({message: 400, });
     })(req, res, next);
   }); */
-  
+
 router.post(
   "/login",
-  passport.authenticate("local",{
+  passport.authenticate("local", {
     successRedirect: "/",
-    failureRedirect: "/login"
+    failureRedirect: "/login",
   })
 );
 
-
 /***** USER LOGOUT*****/
-router.post('/logout', function(req, res, next) {
-  req.logout(function(err) {
-    if (err) { return next(err); }
-    res.redirect('/');
+router.post("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
   });
 });
 
 // Middleware
 function checkAuthenticated(req, res, next) {
-	if (req.isAuthenticated()) {
-	  return res.redirect("/dashboard");
-	}
-	next();
+  if (req.isAuthenticated()) {
+    return res.redirect("/dashboard");
   }
-  
+  next();
+}
+
 function checkNotAuthenticated(req, res, next) {
-  console.log("Check Not Authenticated: ", req.isAuthenticated); 
-	if (req.isAuthenticated()) {
-	  return next();
-	}
-	res.redirect("/login");
+  console.log("Check Not Authenticated: ", req.isAuthenticated);
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
 module.exports = router;
