caf2578193e45c9d76683dc07486b04e72bb0c96
martin0x <raymart.vills@gmail.com>
2023-05-16 01:20:19 +0800

    Fix auth

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -91,7 +91,7 @@ router.post("/register", async (req, res) => {
 });
 
 /**** USER LOGIN ****/
-router.get("/checkAuth", checkAuthenticated, (req, res) => {
+router.get("/checkAuth", (req, res) => {
   // results page
   console.log(req.session);
   console.log(req.sessionID);
@@ -104,7 +104,7 @@ router.get("/checkAuth", checkAuthenticated, (req, res) => {
 });
 
 // Login page for backend testing
-router.get("/login_page", checkAuthenticated, (req, res) => {
+router.get("/login_page", (req, res) => {
   // results page
   res.sendFile("temp_login.html", { root: __dirname });
 });
